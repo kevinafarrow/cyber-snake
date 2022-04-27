38d327d2532da0547c7531ed8ea489b7c81d4fcb
kevinafarrow <github@kfarrow.com>
2022-04-27 16:40:02 -0500

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,140 @@
+<script>
+  import Snake from "./Snake.svelte";
+  import Food from "./Food.svelte";
+  let foodLeft = 0;
+  let foodTop = 0;
+  let direction = 'right';
+  let snakeBodies = [];
+  let ms = 100;
+  let board = {'width': 1250, 'height': 750};
+  let unit = 50;
+
+
+  //draw the game repeatedly
+  setInterval(() => {
+    //console.log('speed: ' + ms);
+    snakeBodies.pop();
+    let { left, top } = snakeBodies[0];
+    const directions = {
+      'up': {'top': -50, 'left': 0},
+      'down': {'top': 50, 'left': 0},
+      'left': {'top': 0, 'left': -50},
+      'right': {'top': 0, 'left': 50}
+    }
+    top += directions[direction]['top']
+    left += directions[direction]['left']
+    //console.log('top: ' + top)
+    //console.log('left: ' + left)
+
+    const newHead = { left, top };
+    snakeBodies = [ newHead, ...snakeBodies];
+
+    // if the snake eats food, create a new food and add a piece to the snake
+    if (isCollide(newHead, { left: foodLeft, top: foodTop})) {
+      moveFood();
+      snakeBodies = [...snakeBodies, snakeBodies[snakeBodies.length - 1]];
+    }
+
+    if (isGameOver()) {
+      alert("Game Over!");
+      resetGame();
+    }
+  }, ms)
+
+  function isCollide(a, b) {
+    return !(
+      a.top < b.top ||
+      a.top > b.top ||
+      a.left < b.left ||
+      a.left > b.left
+    );
+  }
+
+  function moveFood() {
+    foodTop = Math.floor(Math.random() * Math.floor(board.height / unit)) * unit;
+    foodLeft = Math.floor(Math.random() * Math.floor(board.width / unit)) * unit;
+  }
+
+  function isGameOver() {
+    const snakeBodiesNoHead = snakeBodies.slice(1);
+    const snakeCollisions = snakeBodiesNoHead.filter(sb => isCollide(sb, snakeBodies[0]));
+    if (snakeCollisions.length !== 0) {
+      return true;
+    }
+    const { top, left } = snakeBodies[0];
+    if (top >= board.height|| top < 0 || left >= board.width|| left < 0){
+      return true;
+    }
+  }
+
+  function isOpposite(a, b) {
+    const opposites = {
+      'up': 'down',
+      'down': 'up',
+      'left': 'right',
+      'right': 'left'
+    }
+    //console.log('a: ' + a)
+    //console.log('opposite a: ' + opposites[a])
+    //console.log('b: ' + b)
+    //console.log('opposite b: ' + opposites[b])
+    if (opposites[a] == b) {
+      return true
+    }
+  }
+
+  function onKeyDown(e) {
+    console.log(e.keyCode);
+    const newDirection = getDirectionFromKeyCode(e.keyCode);
+    if (!isOpposite(newDirection, direction)) {
+      direction = newDirection;
+    }
+  }
+
+  function resetGame() {
+    moveFood();
+    direction = 'right';
+    snakeBodies = [
+      { left: unit * 2, top: 0 },
+      { left: unit * 1, top: 0 },
+      { left: unit * 0, top: 0 }
+    ]
+  }
+
+  function getDirectionFromKeyCode(keyCode) {
+    const keyTransform = {
+      37: 'left',
+      38: 'up',
+      39: 'right',
+      40: 'down'
+    }
+    return keyTransform[keyCode];
+  }
+  resetGame();
+    //background-image: url("../background.jpg");
+</script>
+
+
+<style>
+  main {
+    width: 1250px;
+    height: 750px;
+    border: solid black 1px;
+    position: relative;
+    margin: 20px auto;
+    background-size: contain;
+    background-color: #333333;
+  }
+  h2,
+  h1 {
+    text-align: center;
+    font-family: 'Roboto Mono', monospace;
+  }
+</style>
+
+<h1>Cyber Snake</h1>
+<main>
+  <Snake {snakeBodies} {direction}/>
+  <Food {foodLeft} {foodTop} />
+</main>
+<svelte:window on:keydown={onKeyDown} />
---
+++ src/SnakeBody.svelte
@@ -0,0 +1,81 @@
+<script>
+  export let top = unit;
+  export let left = unit;
+  export let isHead = false;
+  export let direction = "right";
+</script>
+
+<style>
+  .snake-body {
+    background-color: #00DDDD;
+    border: #00DDDD 1px solid;
+    position: absolute;
+    width: 48px;
+    height: 48px;
+    z-index: 3;
+  }
+  .eyes {
+    position: absolute;
+    background-color: #ADFF2F;
+    border: green 1px solid;
+    border-radius: 50%;
+    width: 20%;
+    height: 20%;
+  }
+  .mouth{
+    position: absolute;
+    background-color: black;
+    border-radius: 30%;
+    width: 20%;
+    height: 80%;
+  }
+  .pupils {
+    position: absolute;
+    background-color: black;
+    border-radius: 50%;
+    width: 5%;
+    height: 5%;
+  }
+  #leftEye {
+    left: 50%;
+    top: 25%;
+  }
+  #rightEye {
+    left: 50%;
+    bottom: 25%;
+  }
+  #leftPupil {
+    left: 63%;
+    top: 33%;
+  }
+  #rightPupil {
+    left: 63%;
+    bottom: 33%;
+  }
+  #mouth {
+    left: 80%;
+    top: 10%;
+  }
+  .snake-body.right {
+    transform: rotate(0deg);
+  }
+  .snake-body.left {
+    transform: rotate(180deg);
+  }
+  .snake-body.up {
+    transform: rotate(270deg);
+  }
+  .snake-body.down {
+    transform: rotate(90deg);
+  }
+</style>
+
+<div class="snake-body {direction}" style="left: {left}px; top: {top}px;">
+  {#if isHead}
+    <div id="leftEye" class="eyes" />
+    <div id="rightEye" class="eyes" />
+    <div id="leftPupil" class="pupils" />
+    <div id="rightPupil" class="pupils" />
+    <div id="mouth" class="mouth" />
+  {/if}
+</div>
